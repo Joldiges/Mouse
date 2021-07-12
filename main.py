456494a3492b4003f9cc28a8f66657fda3a4d47e
james oldiges <jamesoldiges@gmail.com>
2021-07-12 13:09:54 -0400

    init

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ if __name__ == "__main__":
         lastLoc = None
         lastClick = None
         while (time.time() - start) < 14400:  # 4 hours
-        # while (time.time() - start) < 20:  # 20 sec
+        # while (time.time() - start) < 5:  # 20 sec
             now = time.time()
             loc = mouse.get_position()
             leftClick = mouse.is_pressed(button='left')
             if (loc != lastLoc) or (leftClick and not lastClick):
-                print(' '.join([str(i) for i in [now, loc, leftClick]]) + '\n')
-                # f.write(' '.join([str(i) for i in [time.time(), loc, mouse.is_pressed(button='left')]]) + '\n')
+                print('; '.join([str(i) for i in [now, loc, leftClick]]))
+                f.write('; '.join([str(i) for i in [now, loc, leftClick]]) + '\n')
             lastLoc = loc
             lastClick = leftClick
